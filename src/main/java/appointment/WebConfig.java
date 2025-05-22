19c8d12fe91061c10a7038ef1d05fa8eddb1d019
Rooney <walwendarooney@gmail.com>
2025-05-22 19:07:14 +0300

    Review

--- src/main/java/appointment/WebConfig.java
+++ src/main/java/appointment/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns(
-                        "https://mrvisitease.com",
+                        "https://winstec.me",
                         "https://*.vercel.app",  // Now this will work
                         "http://localhost:[*]"  // For any localhost port
                 )
